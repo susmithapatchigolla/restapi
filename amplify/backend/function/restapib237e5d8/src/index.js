b42716e553f6381bf9acab92b6aeca33b1a11baa
Susmitha <susmithapatchigolla99@gmail.com>
2021-10-26 12:17:44 +0530

    12347

--- amplify/backend/function/restapib237e5d8/src/index.js
+++ amplify/backend/function/restapib237e5d8/src/index.js
@@ -4,12 +4,12 @@ exports.handler = async (event) => {
     // TODO implement
     const response = {
         statusCode: 200,
-    //  Uncomment below to enable CORS requests
-    //  headers: {
-    //      "Access-Control-Allow-Origin": "*",
-    //      "Access-Control-Allow-Headers": "*"
-    //  }, 
-        body: JSON.stringify('Hello from Lambda!'),
+     //Uncomment below to enable CORS requests
+     headers: {
+         "Access-Control-Allow-Origin": "*",
+         "Access-Control-Allow-Headers": "*"
+     }, 
+        body: JSON.stringify('Hello from Lambdad!!'),
     };
     return response;
 };
--- src/App.js
+++ src/App.js
@@ -9,21 +9,38 @@ import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
 Amplify.configure(awsconfig);
 
 function App() {
-  useEffect(()=>{
-    const apiName = 'api28ef1e8c ';
-const path = '/items'; 
+  function getData() { 
+    const apiName = 'api28ef1e8c';
+    const path = '/items';
+    
+    console.log("Hel")
+    return API.get(apiName, path);
+    console.log("Hell0")
+  }
+  
+  (async function () {
+    const response = await getData();
+    console.log(response)
+  })();
+//  const abc= () =>{
+//   const apiName = 'api28ef1e8c ';
+//   const path = '/items'; 
 
-    const items = API
-    .get(apiName, path)
-    .then(response => {
-      // Add your code here
-      console.log(response);
-    })
-    .catch(error => {
-      console.log(error.response);
-   });
-    console.log(items);
-  },[])
+//     const items = API
+//     .get(apiName, path)
+//     .then(response => {
+//       // Add your code here
+//       console.log(response);
+//     })
+//     .catch(error => {
+//       console.log(error.response);
+//    });
+//     console.log(items);}
+ 
+
+ 
+  
+    
   return (
     <div className="App">
       <header className="App-header">
@@ -39,7 +56,9 @@ const path = '/items';
         >
           Learn React !
         </a>
+        {}
       </header>
+      <AmplifySignOut />
     </div>
   );
 }
